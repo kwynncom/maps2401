e2a1bf0d83fda5bbe07bae5856529f3e87cef32f
kwynncom <buessresume@gmail.com>
2024-02-05 22:03:58 -0500

    more doc, links, cleaning code

--- index.php
+++ index.php
@@ -6,66 +6,50 @@
 
 <title>Big Evil Goo(gle) Maps polygon</title>
 
-<script src='/opt/kwynn/js/utils.js'></script> <!-- https://github.com/kwynncom/kwynn-php-general-utils -->
+<?php define('KWGM2401_V', '2024/02/05 22:03'); /* not using right now, so not needed, but I find it useful */ ?>
 
-<?php define('KWGM2401_V', '2024/02/05 20:44'); ?>
-
-<script> function initMap() { initMapInner(); } </script>
-
-<!-- https://developers.google.com/maps/documentation/javascript/load-maps-js-api
-	 https://developers.google.com/maps/documentation/javascript/examples/drawing-tools
-	 https://developers.google.com/maps/documentation/javascript/overview#Loading_the_Maps_API -->
+<script> 	
+	function initMap() { initMapActual(); } // defined here so it's above the Goo API inclusion just below; otherwise this function is undefined
+</script>
 
 <?php require_once('getKey.php'); ?>
 <script src="https://maps.googleapis.com/maps/api/js?key=<?php echo(GooMapsAPIKeyAccess2401::get()); ?>&loading=async&callback=initMap&libraries=drawing"></script>
 
-
 <script>
 	
-function initMapInner(){
+function initMapActual(){
 	
-    const map = new google.maps.Map(document.getElementById('map'), 
-	{	center: {lat: 34.2726562, lng: -83.8332973 },
+    const map = new google.maps.Map(document.getElementById('map'), // the element is defined in the body below 
+	{	center: {lat: 34.2726562, lng: -83.8332973 }, // Gainesville, Georgia, USA
         zoom: 12    });
     
 	new google.maps.Marker(
 	{	position: { lat: 34.2696248, lng: -83.819695},
 		map,
 		title: "Hello World!"	});
-  
-// https://developers.google.com/maps/documentation/javascript/examples/drawing-tools
 
 	const drawingManager = new google.maps.drawing.DrawingManager({
 		drawingMode: google.maps.drawing.OverlayType.POLYGON,
 		drawingControl: false  });
 	
-	drawingManager.setMap(map);
+	drawingManager.setMap(map); // associate drawing tools with map
 	
-	// https://developers.google.com/maps/documentation/javascript/drawinglayer
-	// https://stackoverflow.com/questions/14407451/how-to-get-point-coordinates-of-a-modified-drawingmanager-shape-googlemaps-api
 	google.maps.event.addListener(drawingManager, 'polygoncomplete', function(polygon) {
-		const coordinates = (polygon.getPath().getArray());
-		return; });
+		const coordinates = (polygon.getPath().getArray());	 });
 
-	google.maps.event.addListener(drawingManager, 'click', () => {
-		// called before the map listener
-	});
-}  // initMapInner() func
+} // end initMapInner() / Actual func
 </script>
   
 <style>
     body { font-family: sans-serif; }
 
-    #map {
-      width : 40em;
-      height: 35em;
-      border: thin solid black;
-	  text-align: center;
-    }
+    #map {  width : 40em;
+			height: 35em;
+			border: thin solid black;    }
 </style>
 </head>
 <body>
 	<div style='margin: 1em; font-size: 120%;'><a href='./doc/mapsPolygonDoc.html'>discussion</a></div>
-	<div id='map'></div>
+	<div id='map'></div> <!-- THE MAP ITSELF -->
 </body>
 </html>
